c354975129cf13996a14a2584c70f89bb56665a4
Jcat-07 <jec2fi2020@gmail.com>
2021-06-03 12:18:03 -0300

    Inicializacion del Proyecto

---
+++ Proyecto-Final-PySW-BackEnd/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+const {mongoose} = require('./database');
+const cors = require('cors');
+
+//middlewares 
+app.use(express.json()); 
+app.use(cors({origin: 'http://localhost:4200'}));
+
+//Cargamos el modulo de direccionamiento de rutas para puntos 
+//app.use('/api/noticia/'     , require('./routers/noticia.router')); 
+//
+
+//setting 
+app.set('puerto', process.env.PORT || 3000);
+
+//starting the server 
+app.listen(app.get('puerto'), () => { 
+    console.log('Server started on port', app.get('puerto')); 
+});
