fae983fb80bf04b5ddfaa8d4abdddb451d97c962
Mona Lisa <vitobernal@hotmail.com>
2021-06-04 22:12:03 -0300

    agregando contenido

--- Proyecto-Final-PySW-BackEnd/routers/alumnos.router.js
+++ Proyecto-Final-PySW-BackEnd/routers/alumnos.router.js
@@ -2,3 +2,13 @@ const alumnosCtrl = require('../controllers/alumnos.controller');
 //creamos el manejador de rutas
 const express = require('express');
 const router = express.Router();
+
+
+router.post('/', alumnosCtrl.crateAlumno);
+router.get('/', alumnosCtrl.getAlumnos);
+router.get('/:id', alumnosCtrl.getAlumnosparams);
+router.delete('/:id', alumnosCtrl.deleteAlumno);
+router.put('/:id', alumnosCtrl.modificarAlumno);
+
+
+module.exports = router;
