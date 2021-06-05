181fe3d5154d860ced174ae87a611ee4f08b9987
Mona Lisa <vitobernal@hotmail.com>
2021-06-04 21:52:15 -0300

    agregando router

---
+++ Proyecto-Final-PySW-BackEnd/routers/alumnos.router.js
@@ -0,0 +1,4 @@
+const alumnosCtrl = require('../controllers/alumnos.controller');
+//creamos el manejador de rutas
+const express = require('express');
+const router = express.Router();
---
+++ Proyecto-Final-PySW-BackEnd/routers/plan.router.js
@@ -0,0 +1,4 @@
+const planCtrl = require('../controllers/plan.controller');
+//creamos el manejador de rutas
+const express = require('express');
+const router = express.Router();
