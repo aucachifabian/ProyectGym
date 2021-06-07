6c3f72292430b3590bcbc5364077a99ad29274c4
Mona Lisa <vitobernal@hotmail.com>
2021-06-07 19:32:48 -0300

    assistance

--- Proyecto-Final-PySW-FrontEnd/src/app/app-routing.module.ts
+++ Proyecto-Final-PySW-FrontEnd/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AssistanceComponent } from './components/home/coach/register/assistance/assistance.component';
+import { PaymentComponent } from './components/home/coach/register/payment/payment.component';
 import { PupilComponent } from './components/home/coach/register/pupil/pupil.component';
 import { UserComponent } from './components/home/user/user.component';
 
 const routes: Routes = [
- // { path : '', component : PupilComponent},
-  { path : 'Pupil', component : PupilComponent},
-  { path : 'User', component : UserComponent},
-  { path : '**', redirectTo : '/'}
+  // { path : '', component : PupilComponent},
+  { path: 'Pupil', component: PupilComponent },
+  { path: 'User', component: UserComponent },
+  { path: 'Assitance', component: AssistanceComponent },
+  { path: '**', redirectTo: '/' }
 ];
 
 @NgModule({
