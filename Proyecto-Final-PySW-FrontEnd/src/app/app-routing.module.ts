4d22326034dab9bbbd724e39d643a7a72fadd6ec
Mona Lisa <vitobernal@hotmail.com>
2021-06-07 16:26:48 -0300

    users

--- Proyecto-Final-PySW-FrontEnd/src/app/app-routing.module.ts
+++ Proyecto-Final-PySW-FrontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PupilComponent } from './components/home/coach/register/pupil/pupil.component';
+import { UserComponent } from './components/home/user/user.component';
 
 const routes: Routes = [
-  { path : '', component : PupilComponent},
+ // { path : '', component : PupilComponent},
   { path : 'Pupil', component : PupilComponent},
+  { path : 'User', component : UserComponent},
   { path : '**', redirectTo : '/'}
 ];
 
