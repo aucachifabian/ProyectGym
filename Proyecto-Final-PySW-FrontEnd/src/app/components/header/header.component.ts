6555d91998539eeb072392194e05f2cc74240d67
aucachifabian <fabianamericojoel99@gmail.com>
2021-06-25 11:16:36 -0300

    NavBar Student

--- Proyecto-Final-PySW-FrontEnd/src/app/components/header/header.component.ts
+++ Proyecto-Final-PySW-FrontEnd/src/app/components/header/header.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Student } from 'src/app/models/student/student';
 import { LoginService } from 'src/app/services/login/login.service';
+import { StudentService } from 'src/app/services/student/student.service';
 
 @Component({
   selector: 'app-header',
@@ -9,14 +12,37 @@ import { LoginService } from 'src/app/services/login/login.service';
 })
 export class HeaderComponent implements OnInit {
 
-
+  private student : Student;
   /****************************************************/
 
-  constructor(public loginService : LoginService, public router : Router) { 
+  constructor(public loginService : LoginService, public router : Router, private studentService : StudentService,  private toastr : ToastrService) { 
+      this.getStudent();
   }
   /****************************************************/
-
   ngOnInit(): void {
+    this.getStudent();
+  }
+
+  private async getStudent() : Promise<void> {
+    if(this.loginService.isLogged()=='student')
+    {
+      this.student = new Student();
+    await this.studentService.getStudentByDni(this.loginService.isOwner()).subscribe(
+      result => {
+        if(result.status == "1"){
+          alert(result.student)
+          Object.assign(this.student,result.student);
+        }
+        else {
+          this.toastr.error(result.msg,"Error");
+        }
+      },
+
+      error => {
+        this.toastr.error("ERROR","ERROR");
+      }
+    );
+    }
   }
 
   /****************************************************/
@@ -25,5 +51,22 @@ export class HeaderComponent implements OnInit {
     this.loginService.logout();
   }
 
-  /****************************************************/
+   /***************************************************/
+
+   public checkAssistance() : void {
+    this.router.navigate(["student/assistance",this.student._id]);
+  }
+
+  /***************************************************/
+
+  public checkPayment() : void {
+    this.router.navigate(["student/payment",this.student._id]);
+
+  }
+
+  /***************************************************/
+
+  public checkRoutine() : void {
+    this.router.navigate(["student/routine",this.student._id]);
+  }
 }
